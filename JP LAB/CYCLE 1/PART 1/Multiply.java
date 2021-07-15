70b29040a9f534ff021d6f37ed84e39195cba0b4
1a1242 <85673838+1a1242@users.noreply.github.com>
2021-07-15 11:56:32 +0530

    Create Multiply.java

---
+++ JP LAB/CYCLE 1/PART 1/Multiply.java
@@ -0,0 +1,7 @@
+public class Multiply{
+       public static void main(String args[]){
+              int a=5, b=3, c;
+              c = a*b;
+              System.out.println("Multiplication of numbers is = " +c);
+       }
+}
